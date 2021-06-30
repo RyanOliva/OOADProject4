5c66aebf79de7a2099622b370e53b0c7d29bfa56
bouldercoder9 <alma9011@colorado.edu>
2021-06-30 17:54:50 -0600

    Made first real test

--- src/tests/MyUnitTest.java
+++ src/tests/MyUnitTest.java
@@ -1,13 +1,25 @@
 package tests;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import util.*;
+import Garage.*;
 
 public class MyUnitTest 
 {
+    // test number 1
     @Test
-    public void test ()
+    public void verifyUserInstantiation ()
     {
-        System.out.println("this test ran");
-        assertEquals(6, 4);
+        InputStream backup = System.in;
+        ByteArrayInputStream simulatedInput = new ByteArrayInputStream("test name".getBytes());
+        System.setIn(simulatedInput);
+
+        Garage testGarage = new Garage ();
+        assertEquals("test name", User.getUserInstance (testGarage).getName());
+
+        System.setIn(backup);
     }
 }
